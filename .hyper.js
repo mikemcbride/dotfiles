7c9e8a2f78198f60827dc1ca68dd288c05d38e69
Mike McBride <mmcbride1007@gmail.com>
2017-01-19 09:29:12 -0600

    use theme plugin vs custom css

--- .hyper.js
+++ .hyper.js
@@ -1,85 +1,16 @@
 'use strict'
 
-const black = '#000'
-const white = '#bbb'
-const red = '#ff2e88'
-const green = '#50e3c2'
-const cyan = green
-const yellow = '#fff'
-const blue = '#fff'
-const magenta = '#fff'
-const lightWhite = '#fff'
-const lightBlack = '#6c6c6c'
-
-const colors = {
-  black,
-  red,
-  green,
-  yellow,
-  blue,
-  magenta,
-  cyan,
-  white,
-  lightBlack,
-  lightRed: red,
-  lightGreen: green,
-  lightYellow: yellow,
-  lightBlue: blue,
-  lightMagenta: magenta,
-  lightCyan: cyan,
-  lightWhite
-}
-
-const cursorColor = 'rgb(248,29,229)'
-const borderColor = '#444'
-
-const tabText = white
-const tabTextActive = '#fff'
-const dividerBg = 'rgba(255,255,255,.1)'
-
 // build config object
 const config = {
   fontSize: 14,
-  cursorShape: 'UNDERLINE',
-  foregroundColor: white,
-  backgroundColor: black,
-  borderColor,
-  cursorColor,
-  colors,
-  termCSS: `
-    .cursor-node {
-      mix-blend-mode: difference;
-    }
-  `,
-  css: `
-    .tabs_list {
-      margin-left: 0;
-    }
-    .tab_tab.tab_first {
-      padding-left: 82px;
-    }
-    .tab_textInner {
-      color: ${tabText};
-    }
-    .tab_tab.tab_active .tab_textInner {
-      color: ${tabTextActive};
-    }
-    .tab_firstActive {
-      border-left-width: 0px;
-    }
-    .tabs_borderShim {
-      border-width: 0 !important;
-    }
-    .splitpane_divider {
-      background-color: ${dividerBg} !important;
-    }
-  `
+  fontFamily: 'Hack, SF Mono, Menlo, DejaVu Sans Mono, Lucida Console, monospace',
+  cursorShape: 'BLOCK',
 }
 
 // a list of plugins to fetch and install from npm
 const plugins = [
-  // 'hyper-website-theme',
-  // 'hyper-electron-highlighter',
+  'hyper-website-theme',
+  'hypercwd',
   'hyperlinks',
   'hyper-font-smoothing',
   'hyperterm-lastpass'
