34d691a79d3b10c18307cd380bfdcb2214d73438
Mike McBride <mmcbride1007@gmail.com>
2017-05-21 23:39:03 -0500

    update WWT git config script to only execute if in a git repo

--- scripts/setWwtGitConfig.sh
+++ scripts/setWwtGitConfig.sh
@@ -2,7 +2,14 @@
 # it will cd into each repo and set the git config info to be wwt email
 for d in */*/; do
   cd $d;
-  git config user.name "Mike McBride";
-  git config user.email "mike.mcbride@wwt.com";
+  
+  # check if we are in a git repo
+  # if so, update config
+  if git rev-parse --is-inside-work-tree &>/dev/null; then
+    git config user.name "Mike McBride";
+    git config user.email "mike.mcbride@wwt.com";
+	fi
+  
+  # get back to where we started so the loop doesn't break
   cd ../..;
 done
