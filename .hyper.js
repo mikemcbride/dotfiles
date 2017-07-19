380383367cb1ae0bb1ff9ff09e83048c755ce4fd
Mike McBride <mmcbride1007@gmail.com>
2017-07-19 14:09:38 -0500

    bump hyper font size, remove unused plugin config

--- .hyper.js
+++ .hyper.js
@@ -3,7 +3,7 @@
 module.exports = {
   config: {
     shell: '/bin/zsh',
-    fontSize: 14,
+    fontSize: 16,
     fontFamily: 'Hack, SF Mono',
     cursorShape: 'BLOCK',
     termCSS: `
@@ -18,25 +18,10 @@ module.exports = {
     `,
     hyperlinks: {
       defaultBrowser: true
-    },
-    paneNavigation: {
-      hotkeys: {
-        navigation: {
-          up: 'alt+shift+up',
-          down: 'alt+shift+down',
-          left: 'alt+shift+left',
-          right: 'alt+shift+right'
-        },
-        jump_prefix: 'alt+shift',
-        permutation_modifier: 'ctrl',
-        maximize: 'meta+enter'
-      },
-      showIndicators: false
     }
   },
   plugins: [
     'hyper-electron-highlighter',
-    // 'hyper-website-theme',
     'hypercwd',
     'hyper-hide-title',
     'hyperlinks',
