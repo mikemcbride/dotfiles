ed5e6247cd9d7b08568341f3ffc52a6cb96540ad
Mike McBride <mmcbride1007@gmail.com>
2017-09-26 15:46:12 -0500

    update mono3, add some comments to hyper config

--- .hyper.js
+++ .hyper.js
@@ -3,16 +3,18 @@
 // color definitions
 const mono1 = '#181818'
 const mono2 = '#383838'
-const mono3 = '#585858'
+const mono3 = '#686868'
 const mono4 = '#989898'
 const mono5 = '#b8b8b8'
 const mono6 = '#d8d8d8'
 const mono7 = '#f8f8f8'
 
 // only red and green are not mono-chrome
+// this is helpful for many testing, linting, etc. things that are printed to the console.
 const red = '#ff5869'
 const green = '#6af699'
 
+// config settings
 const cursorColor = green
 const borderColor = mono2
 const tabText = mono4
