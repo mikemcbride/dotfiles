c5dd7355a4a7bdd19b7948161d5bc804a639e001
Mike McBride <mmcbride1007@gmail.com>
2018-01-25 13:44:12 -0600

    allow openGitRepo to open GitLab URLs

--- scripts/openGitRepo.js
+++ scripts/openGitRepo.js
@@ -5,13 +5,15 @@ const execa = require('execa')
 const isGit = require('is-git-repository')
 const open = require('open')
 
-async function openGitHub() {
+async function openGit() {
   if (isGit()) {
     const remoteOrigin = await execa('git', ['config', '--get', 'remote.origin.url'])
-    const httpRegex = /http(s)?:\/\/github\./gi
+    const httpRegex = /http(s)?:\/\/(github|gitlab)\./gi
     const githubSshRegex = /git\@github\.com:/gi
+    const gitlabSshRegex = /git\@gitlab\.com:/gi
     const wwtSshRegex = /git\@github\.wwt\.com:/gi
     const githubHttp = 'https://github.com/'
+    const gitlabHttp = 'https://gitlab.com/'
     const wwtHttp = 'https://github.wwt.com/'
     const gitUrl = remoteOrigin.stdout.replace(/\.git$/gi, '') // remove trailing '.git' from url
     
@@ -19,10 +21,13 @@ async function openGitHub() {
       // it's already an http link, just open it.
       open(gitUrl)
     } else if (gitUrl.match(githubSshRegex)) {
-      // public github
+      // public GitHub
       open(gitUrl.replace(githubSshRegex, githubHttp))
-    } else if (gitUrl.match(wwtSshRegex)) {
-      // WWT github
+    } else if (gitUrl.match(gitlabSshRegex)) {
+      // publig GitLab
+      open(gitUrl.replace(gitlabSshRegex, gitlabHttp))
+    }else if (gitUrl.match(wwtSshRegex)) {
+      // WWT GitHub
       open(gitUrl.replace(wwtSshRegex, wwtHttp))
     }
   } else {
@@ -33,4 +38,4 @@ async function openGitHub() {
   return
 }
 
-openGitHub()
+openGit()
