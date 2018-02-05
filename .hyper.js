d2bf81f52b07a523d602eb241545af066846a7d4
Mike McBride <mmcbride1007@gmail.com>
2018-02-05 09:30:11 -0600

    remove border

--- .hyper.js
+++ .hyper.js
@@ -5,21 +5,12 @@ module.exports = {
     updateChannel: 'canary',
     shell: '/bin/zsh',
     fontSize: 14,
-    fontFamily: 'Fira Code, Fira Mono, Operator Mono, Hack, SF Mono',
+    fontFamily: 'Fira Mono, Operator Mono, Hack, SF Mono',
     cursorShape: 'BLOCK',
     css: `
       body {
         -webkit-backface-visibility: unset;
         backface-visibility: unset;
-      }
-      .hyper_main {
-        border-width: 2px !important;
-        border-color: #4ff2f8 !important;
-      }
-      .header_header {
-        top: 2px;
-        left: 2px;
-        right: 2px;
       }
     `,
     hyperlinks: {
