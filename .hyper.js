7503ce6996c7d96130536d3e34acf3d0a2e7abb5
Mike McBride <mmcbride1007@gmail.com>
2018-02-07 10:34:03 -0600

    include `now` in Brewfile

--- .hyper.js
+++ .hyper.js
@@ -20,8 +20,8 @@ module.exports = {
   plugins: [
     'hypercwd',
     // 'hyper-mono-christmas',
-    // 'hyper-electron-highlighter',
-    'hyper-firefox-devtools',
+    'hyper-electron-highlighter',
+    // 'hyper-firefox-devtools',
     'hyper-hide-title',
     'hyperlinks',
     'hyper-font-smoothing',
