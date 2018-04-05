01fce774b161622421862051e27d0aac116b9e1a
Mike McBride <mmcbride1007@gmail.com>
2018-04-05 10:45:50 -0500

    modify prompt to show only two directories

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,35 @@
 function fish_prompt
   # blue working directory
   set_color blue
-  echo -n (prompt_pwd)
+  
+  
+  # replace $HOME with ~
+  set realhome ~
+  set tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+  set dirs_arr (string split '/' $tmp)
+  set dirs_len (count $dirs_arr)
+  
+  if test $dirs_len -le 2
+    # directory path is less than or equal to 2
+    # just print it out
+    echo -n $tmp
+  else
+    for i in (seq $dirs_len)
+      if test $i -eq (math $dirs_len - 1)
+        echo -n $dirs_arr[$i]
+      else if test $i -eq $dirs_len
+        echo -n "/$dirs_arr[$i]"
+      end
+    end
+  end
   
   # get the current git branch
-  set -l branch (vcprompt -f '%b')
+  set branch (vcprompt -f '%b')
   
-  if test $branch != ''
-    set -l gitstatus (vcprompt -f '%m')
+  if test -n "$branch"
+    set gitstatus (vcprompt -f '%m')
     # we have a git branch
-    if test $gitstatus
+    if test -n "$gitstatus"
       # git status is dirty
       set_color yellow
     else
