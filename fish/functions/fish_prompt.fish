42b6392daeccae3a2491c37e7adaf469b570aac0
Mike McBride <mmcbride1007@gmail.com>
2018-04-08 23:54:41 -0500

    extract title and prompt logic into external function

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,26 +2,7 @@ function fish_prompt
   # blue working directory
   set_color blue
   
-  
-  # replace $HOME with ~
-  set realhome ~
-  set tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
-  set dirs_arr (string split '/' $tmp)
-  set dirs_len (count $dirs_arr)
-  
-  if test $dirs_len -le 2
-    # directory path is less than or equal to 2
-    # just print it out
-    echo -n $tmp
-  else
-    for i in (seq $dirs_len)
-      if test $i -eq (math $dirs_len - 1)
-        echo -n $dirs_arr[$i]
-      else if test $i -eq $dirs_len
-        echo -n "/$dirs_arr[$i]"
-      end
-    end
-  end
+  echo -n (pwd_plus_one)
   
   # get the current git branch
   set branch (vcprompt -f '%b')
---
+++ fish/functions/pwd_plus_one.fish
@@ -0,0 +1,21 @@
+function pwd_plus_one
+  # replace $HOME with ~
+  set realhome ~
+  set tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+  set dirs_arr (string split '/' $tmp)
+  set dirs_len (count $dirs_arr)
+  
+  if test $dirs_len -le 2
+    # directory path is less than or equal to 2
+    # just print it out
+    echo -n $tmp
+  else
+    for i in (seq $dirs_len)
+      if test $i -eq (math $dirs_len - 1)
+        echo -n $dirs_arr[$i]
+      else if test $i -eq $dirs_len
+        echo -n "/$dirs_arr[$i]"
+      end
+    end
+  end
+end
