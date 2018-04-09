042113559e2acdd89120249ce7298ecb83e88e2b
Mike McBride <mmcbride1007@gmail.com>
2018-04-09 00:16:36 -0500

    use vi keybindings in prompt

---
+++ fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,4 @@
+function fish_mode_prompt --description 'override builtin fish_mode_prompt'
+  # this override is here so that in fish_vi_mode, the annoying mode indicator goes away.
+  # without this override, you will see an indicator to the left of the prompt showing which vi mode you are in.
+end function
