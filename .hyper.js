e3c8dcb77149b42b3f01a896cdea2c40b7d89bad
Mike McBride <mmcbride1007@icloud.com>
2018-09-19 10:03:57 -0500

    update prompt, hyper colors

--- .hyper.js
+++ .hyper.js
@@ -1,5 +1,49 @@
 'use strict'
 
+// will build an array of HSL values with the same hue and
+// saturation values, and lightness values going from 10% -100%
+// then we just reference the index in the array for our desired color.
+let mono = Array(7).fill().map((x,i) => `hsl(221, 6%, ${10 + (i*15)}%)`)
+
+const black = mono[0]
+const white = mono[6]
+const lightBlack = mono[2]
+
+const red = '#ff5869'
+const green = '#6af699'
+const yellow = mono[4]
+const blue = mono[5]
+const magenta = mono[5]
+const cyan = mono[4]
+
+const colors = {
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  lightBlack,
+  lightRed: red,
+  lightGreen: green,
+  lightYellow: yellow,
+  lightBlue: blue,
+  lightMagenta: magenta,
+  lightCyan: cyan,
+  lightWhite: white 
+}
+
+const backgroundColor = black
+const foregroundColor = mono[3]
+const selectionColor = 'hsla(221, 90%, 75%, 0.15)'
+const tabText = mono[2]
+const tabTextActive = white
+const dividerBg = mono[1]
+const cursorColor = green
+const borderColor = mono[1]
+
 module.exports = {
   config: {
     updateChannel: 'canary',
@@ -10,28 +54,34 @@ module.exports = {
     fontWeightBold: 'normal',
     fontFamily: 'Fira Mono',
     cursorShape: 'UNDERLINE',
-    cursorColor: '#34febb',
+    selectionColor,
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
     css: `
-      body {
-        -webkit-backface-visibility: unset;
-        backface-visibility: unset;
+      .tabs_title {
+  			display: none !important;
+  		}
+      .tab_textInner {
+        color: ${tabText};
+        font-weight: 500;
       }
-      .tab_tab.tab_active:after {
-        content: '';
-        pointer-events: none;
-        z-index: 2;
-        position: absolute;
-        top: -1px;
-        left: 0;
-        bottom: 0;
-        width: 2px;
-        background-color: #568af2;
+      .tab_tab.tab_active {
+        background-color: ${backgroundColor};
+      }
+      .tab_tab.tab_active .tab_textInner {
+        color: ${tabTextActive};
+      }
+      .splitpane_divider {
+        background-color: ${dividerBg} !important;
       }
     `
   },
   plugins: [
     'hypercwd',
-    'hyper-electron-highlighter',
+    // 'hyper-electron-highlighter',
     'hyper-hide-title',
   ],
   localPlugins: [],
