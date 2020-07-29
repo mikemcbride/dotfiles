4bc2f67269ccfce3b309d6657e6b22635afce4d6
Mike McBride <mike.mcbride@hey.com>
2020-07-29 15:26:14 -0500

    comment out fish_prompt since we are using starship

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,35 +1,35 @@
 function fish_prompt
-  # lightblack timestamp
-  set_color brblack
+  # # lightblack timestamp
+  # set_color brblack
 
-  set cur_time (date +%T)
-  echo -n "[$cur_time] "
+  # set cur_time (date +%T)
+  # echo -n "[$cur_time] "
 
-  # blue working directory
-  set_color blue
+  # # blue working directory
+  # set_color blue
   
-  # function to get current working directory plus one level up
-  echo -n (base_dir)
+  # # function to get current working directory plus one level up
+  # echo -n (base_dir)
   
-  # get the current git branch, using vcprompt (installed via homebrew)
-  set branch (vcprompt -f '%b')
+  # # get the current git branch, using vcprompt (installed via homebrew)
+  # set branch (vcprompt -f '%b')
   
-  if test -n "$branch"
-    # we have a git branch
+  # if test -n "$branch"
+  #   # we have a git branch
 
-    set gitstatus (vcprompt -f '%m')
-    set_color brblack
+  #   set gitstatus (vcprompt -f '%m')
+  #   set_color brblack
 
-    if test -n "$gitstatus"
-      # git status is dirty
-      set_color yellow
-    end
+  #   if test -n "$gitstatus"
+  #     # git status is dirty
+  #     set_color yellow
+  #   end
     
-    # now that we have set our color, print the git branch.
-    echo -n " [$branch]"
-  end
+  #   # now that we have set our color, print the git branch.
+  #   echo -n " [$branch]"
+  # end
   
-  set_color purple
-  echo -n " » "
-  set_color normal
+  # set_color purple
+  # echo -n " » "
+  # set_color normal
 end
