bf5b12d8ef5b0470e441aa78fee4298707b848f5
Mike McBride <mike.mcbride@hey.com>
2020-10-07 15:14:37 -0500

    update toggleThemes to use os.homedir

--- scripts/toggleThemes.js
+++ scripts/toggleThemes.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const { promisify } = require('util')
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
+const os = require('os')
+const path = require('path')
+const util = require('util')
 
-const hyperConfigFile = '/Users/mcbridem/src/dotfiles/.hyper.js'
-const vsCodeConfigFile = '/Users/mcbridem/Library/Application\ Support/Code/User/settings.json'
+const homedir = os.homedir()
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
+const hyperConfigFile = path.join(homedir, 'src/dotfiles/.hyper.js')
+const vsCodeConfigFile = path.join(homedir, 'Library/Application\ Support/Code/User/settings.json')
 
 readFile(hyperConfigFile, 'utf8').then(res => {
     let isDark = res.includes('hyper-electron-highlighter')
