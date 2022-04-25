70b1ebaec39d8c5a4deaa30ae6e6e0005c68ee18
Mike McBride <mike.mcbride@hey.com>
2022-04-25 10:22:05 -0500

    nvim stuff

--- .hyper.js
+++ .hyper.js
@@ -1,20 +1,21 @@
 'use strict'
 
 module.exports = {
-  config: {
-    preserveCWD: true,
-    shell: '/opt/homebrew/bin/fish',
-    scrollback: 10000,
-    fontSize: 14,
-    lineHeight: 1.3,
-    fontWeight: 'normal',
-    fontWeightBold: 'normal',
-    fontFamily: 'MonoLisa',
-    cursorShape: 'BLOCK',
-    selectionColor: 'rgba(33, 40, 54, .5)'
-  },
-  plugins: [
-    'hyper-electron-highlighter'
-  ],
-  localPlugins: []
+    config: {
+        updateChannel: 'canary',
+        preserveCWD: true,
+        shell: '/opt/homebrew/bin/fish',
+        scrollback: 10000,
+        fontSize: 14,
+        lineHeight: 1.3,
+        fontWeight: 'normal',
+        fontWeightBold: 'normal',
+        fontFamily: 'MonoLisa',
+        cursorShape: 'BLOCK',
+        selectionColor: 'rgba(33, 40, 54, .5)'
+    },
+    plugins: [
+        'hyper-electron-highlighter'
+    ],
+    localPlugins: []
 }
--- fish/functions/zed.fish
+++
@@ -1,7 +0,0 @@
-function zed
-  if test -n "$argv"
-    open -a "Zed" $argv
-  else
-    open -a "Zed" $PWD
-  end
-end
