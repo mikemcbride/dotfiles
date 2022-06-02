89dbd1b9b7956d6c237490cce23bb2a0af8da230
Mike McBride <mike.mcbride@hey.com>
2022-06-01 21:59:07 -0500

    adds treesitter-context plugin HOLY SMOKES you gotta try it

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -100,6 +100,7 @@ return packer.startup(function(use)
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate",
     }
+    use 'nvim-treesitter/nvim-treesitter-context' -- pin method signatures to top of screen
     use "JoosepAlviste/nvim-ts-context-commentstring"
 
     -- Git
