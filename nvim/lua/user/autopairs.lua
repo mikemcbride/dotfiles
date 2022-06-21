739f0963a50e528b68276df0ac82f8eff640042a
Mike McBride <mike.mcbride@hey.com>
2022-06-21 00:10:12 -0500

    don't use implicit require function call

--- nvim/lua/user/autopairs.lua
+++ nvim/lua/user/autopairs.lua
@@ -25,7 +25,7 @@ npairs.setup {
   },
 }
 
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   return
