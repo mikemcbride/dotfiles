515afdbe0f3d5a2fc0f842d3fd00e6de0fc3f507
Mike McBride <mike.mcbride@hey.com>
2022-06-21 00:03:44 -0500

    add yanked text highlight, update brewfile

---
+++ nvim/lua/user/autocommands.lua
@@ -0,0 +1,16 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+-- briefly highlight yanked text
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 60,
+        })
+    end,
+})
+
