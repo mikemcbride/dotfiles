d833968fc095a328563f7dc955a8b49b1ef048a9
Mike McBride <mike.mcbride@hey.com>
2022-09-23 14:57:55 -0500

    remove null-ls

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -11,11 +11,6 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
-
 	 if server.name == "sumneko_lua" then
 	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
 	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
