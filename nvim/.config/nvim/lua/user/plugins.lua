f6e2287de890592e03bffd2cec74d7243f308718
Mike McBride <mike.mcbride@hey.com>
2022-09-23 14:59:24 -0500

    remove null-ls plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -88,7 +88,7 @@ return packer.startup(function(use)
     use "neovim/nvim-lspconfig" -- enable LSP
     use "williamboman/nvim-lsp-installer" -- simple to use language server installer
     use "tamago324/nlsp-settings.nvim" -- language server settings defined in json
-    use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+    -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
     use {
       "folke/trouble.nvim",
       requires = "kyazdani42/nvim-web-devicons",
