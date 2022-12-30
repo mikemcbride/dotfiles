87eed0ba69cd7f6013cb8bcfcbbe07c5e8b959fb
Mike McBride <mike.mcbride@hey.com>
2022-12-30 02:26:10 -0600

    add function to delete squashed branches

---
+++ fish/.config/fish/functions/delete_squashed_branches.fish
@@ -0,0 +1,3 @@
+function delete_squashed_branches
+  git-delete-squashed
+end
--- fish/.config/fish/functions/gcl.fish
+++ fish/.config/fish/functions/gcl.fish
@@ -1,6 +1,3 @@
 function gcl
-  # for now, the remote thing isn't working super consistently.
-  # I'll only delete locally merged branches
-  # delete_remote_merged_branches; and delete_local_merged_branches
-  delete_local_merged_branches
+  delete_local_merged_branches; and delete_squashed_branches
 end
