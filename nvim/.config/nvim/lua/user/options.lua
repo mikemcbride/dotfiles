b36094709bc781a06a1c903edd02cdae585425bf
Mike McBride <mike.mcbride@hey.com>
2023-01-08 09:46:07 -0600

    add ripgrep config, modify tmux

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -27,80 +27,80 @@ vim.o.termguicolors = true
 vim.o.completeopt = 'menuone,noselect'
 
 -- creates a backup file
-vim.o.backup = false 
+vim.o.backup = false
 
 -- allows neovim to access the system clipboard
-vim.o.clipboard = "unnamedplus" 
+vim.o.clipboard = "unnamedplus"
 
 -- set the height of the command line to 1 row
-vim.o.cmdheight = 1 
+vim.o.cmdheight = 1
 
 -- so that `` is visible in markdown files
-vim.o.conceallevel = 0 
+vim.o.conceallevel = 0
 
 -- the encoding written to a file
-vim.o.fileencoding = "utf-8" 
+vim.o.fileencoding = "utf-8"
 
 -- never allow the mouse to be used in neovim
-vim.o.mouse = "" 
+vim.o.mouse = ""
 
 -- disable the indent guide/ruler
-vim.o.ruler = false 
+vim.o.ruler = false
 
 -- pop up menu height
-vim.o.pumheight = 10 
+vim.o.pumheight = 10
 
 -- we don't need to see things like -- INSERT -- anymore
-vim.o.showmode = false 
+vim.o.showmode = false
 
 -- always show tabs
-vim.o.showtabline = 2 
+vim.o.showtabline = 2
 
 -- make indenting smarter again
-vim.o.smartindent = true 
+vim.o.smartindent = true
 
 -- force all horizontal splits to go below current window
-vim.o.splitbelow = true 
+vim.o.splitbelow = true
 
 -- force all vertical splits to go to the right of current window
-vim.o.splitright = true 
+vim.o.splitright = true
 
 -- creates a swapfile
-vim.o.swapfile = false 
+vim.o.swapfile = false
 
 -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.o.timeoutlen = 500 
+vim.o.timeoutlen = 500
 
 -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-vim.o.writebackup = false 
+vim.o.writebackup = false
 
 -- convert tabs to spaces
-vim.o.expandtab = true 
+vim.o.expandtab = true
 
 -- highlight the current line
-vim.o.cursorline = false 
+vim.o.cursorline = false
 
 -- set numbered lines
-vim.o.number = true 
+vim.o.number = true
 
 -- set relative numbered lines
-vim.o.relativenumber = true 
+vim.o.relativenumber = true
 
 -- set number column width to 4
-vim.o.numberwidth = 4 
+vim.o.numberwidth = 4
 
 -- always show the sign column, otherwise it would shift the text each time
 vim.o.signcolumn = "yes"
 
--- display lines as one long line
-vim.o.wrap = false 
+-- wrap text
+vim.o.wrap = true
 
--- show a wrap guide at 120 characters (but we don't wrap)
-vim.wo.colorcolumn = "120" 
+-- show a wrap guide at 120 characters
+vim.wo.colorcolumn = "120"
 
 -- start scrolling when you are this many lines from the bottom/top of the screen
-vim.o.scrolloff = 8 
+vim.o.scrolloff = 8
 
 -- start scrolling when you are this many columns from the left/right of the screen
-vim.o.sidescrolloff = 8 
+vim.o.sidescrolloff = 8
 
