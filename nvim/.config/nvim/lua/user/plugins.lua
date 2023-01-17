8dd5f5cec7dcca64b322e97a068cfe9c82b83d52
Mike McBride <mike.mcbride@hey.com>
2023-01-17 11:51:50 -0600

    update how colorscheme is loaded

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,5 +1,22 @@
 return {
   {
+    'folke/tokyonight.nvim',
+    lazy = false, -- make sure we load this during startup
+    priority = 1000, -- make sure we load this before all the other start plugins
+    config = function()
+      require('tokyonight').setup({
+        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        styles = {
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "transparent", -- style for sidebars, see below
+          floats = "transparent", -- style for floating windows
+        }
+      })
+      vim.cmd([[colorscheme tokyonight-night]])
+    end
+  },
+  {
     'VonHeikemen/lsp-zero.nvim',
     dependencies = {
       -- LSP Support
@@ -8,7 +25,7 @@ return {
       'williamboman/mason-lspconfig.nvim',
       'j-hui/fidget.nvim',
       'folke/neodev.nvim',
-  
+
       -- Autocompletion
       'hrsh7th/nvim-cmp',
       'hrsh7th/cmp-buffer',
@@ -16,7 +33,7 @@ return {
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
-  
+
       -- Snippets
       'L3MON4D3/LuaSnip',
       'rafamadriz/friendly-snippets',
@@ -37,11 +54,6 @@ return {
   'tpope/vim-fugitive',
   'lewis6991/gitsigns.nvim',
 
-  {
-    'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup
-    priority = 1000, -- make sure we load this before all the other start plugins
-  },
   'nvim-lualine/lualine.nvim',
   'lukas-reineke/indent-blankline.nvim',
   'numToStr/Comment.nvim',
