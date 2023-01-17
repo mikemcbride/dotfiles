eaace56da84ad73e4c16d7884c851351b700263a
Mike McBride <mike.mcbride@hey.com>
2023-01-17 11:44:28 -0600

    migrate from packer to lazy.nvim

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,9 +1,4 @@
 -- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,66 +1,72 @@
-return function(use)
-  -- Package manager
-  use 'wbthomason/packer.nvim'
-  
-  use {
+return {
+  {
     'VonHeikemen/lsp-zero.nvim',
-    requires = {
+    dependencies = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-      {'j-hui/fidget.nvim'},
-      {'folke/neodev.nvim'},
+      'neovim/nvim-lspconfig',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'j-hui/fidget.nvim',
+      'folke/neodev.nvim',
   
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
   
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
     }
-  }
+  },
 
 
-  use { -- Highlight, edit, and navigate code
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
-  }
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    }
+  },
 
-  use { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
+  'tpope/vim-fugitive',
+  'lewis6991/gitsigns.nvim',
 
-  -- Git related plugins
-  use 'tpope/vim-fugitive'
-  use 'lewis6991/gitsigns.nvim'
-
-  use 'folke/tokyonight.nvim'
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  {
+    'folke/tokyonight.nvim',
+    lazy = false, -- make sure we load this during startup
+    priority = 1000, -- make sure we load this before all the other start plugins
+  },
+  'nvim-lualine/lualine.nvim',
+  'lukas-reineke/indent-blankline.nvim',
+  'numToStr/Comment.nvim',
+  'tpope/vim-sleuth',
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+  -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    cond = vim.fn.executable 'make' == 1
+  },
 
-  use "akinsho/bufferline.nvim" -- buffers appear like tabs
-  use "moll/vim-bbye" -- close buffers
-  use "goolord/alpha-nvim" -- dashboard
-  use "fedepujol/move.nvim" -- move lines
-  use "nvim-treesitter/nvim-treesitter-context" -- sticky function headers
-  use "JoosepAlviste/nvim-ts-context-commentstring"
-  use "kyazdani42/nvim-web-devicons"
-  use "windwp/nvim-autopairs" -- automatically close matching brackets/parens/etc
-end
+  "akinsho/bufferline.nvim", -- buffers appear like tabs
+  "moll/vim-bbye", -- close buffers
+  "goolord/alpha-nvim", -- dashboard
+  "ur4ltz/move.nvim", -- move lines
+  "nvim-treesitter/nvim-treesitter-context", -- sticky function headers
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  "kyazdani42/nvim-web-devicons",
+  "windwp/nvim-autopairs", -- automatically close matching brackets/parens/etc
+}
