d79a690f3e408b73cc26d0689b2481076bd20e0e
Mike McBride <mike.mcbride@hey.com>
2023-03-07 08:31:51 -0600

    clean up some nvim keymaps, other stuff

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'go', 'lua', 'javascript', 'html', 'typescript', 'help' },
+  ensure_installed = { 'go', 'lua', 'javascript', 'html', 'typescript', 'help', 'svelte', 'vue' },
 
 
   highlight = { enable = true },
