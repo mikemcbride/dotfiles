f4860adc71799c6d790dad8cab061016a1b7021e
Mike McBride <mike.mcbride@hey.com>
2023-04-17 15:01:26 -0500

    update all git checkout references to be git switch

---
+++ nvim/.config/nvim/after/plugin/noice.lua
@@ -0,0 +1,15 @@
+-- local status_ok, noice = pcall(require, "noice")
+-- if not status_ok then
+--   return
+-- end
+--
+-- noice.setup {
+--   lsp = {
+--     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--     override = {
+--       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--       ["vim.lsp.util.stylize_markdown"] = true,
+--       ["cmp.entry.get_documentation"] = true,
+--     },
+--   },
+-- }
