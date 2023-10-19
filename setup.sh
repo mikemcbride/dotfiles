0b740193f9d20781f5a4282fc6408594e38cd9a2
Mike McBride <mike.mcbride@hey.com>
2023-10-18 22:33:02 -0500

    update readme to reflect change in dotfiles directory location

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 sudo mkdir -p /usr/local/{bin,lib,include,share}
 sudo chown -R $(whoami) /usr/local/{bin,lib,include,share}
-rm -rf ~/.config
 npm i -g empty-trash-cli fkill-cli np trash-cli convert-color-cli yarn vtop
-cd ~
+mkdir -p ~/personal
+cd ~/personal
 git clone https://github.com/mikemcbride/dotfiles.git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap Homebrew/bundle
-cd ~/dotfiles && brew bundle
+cd ~/personal/dotfiles && brew bundle
 rm ~/.gitconfig
-stow {nvim,bat,karabiner,starship,git,zed,bin,ripgrep,zsh,fish}
+stow -t ~ {nvim,bat,karabiner,starship,git,zed,bin,ripgrep,fish,zsh,tmux,ytdl}
 go install github.com/tj/node-prune@latest
 npm install
-node ~/dotfiles/scripts/setDefaultApplications.js
+node ~/personal/dotfiles/scripts/setDefaultApplications.js
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux new-session
