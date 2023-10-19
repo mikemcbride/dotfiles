42557924b554901cbf993bbe94750d171b6b3a13
Mike McBride <mike.mcbride@hey.com>
2023-10-18 22:04:17 -0500

    housekeeping

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -5,22 +5,12 @@ vim.cmd.colorscheme "default"
 
 -- local theme = vim.env.CURRENT_THEME
 
--- if (theme == "gruvbox")
+-- if (theme == "eighties")
 -- then
---   require("gruvbox_material").setup({
---     transparent_mode = true,
---     contrast = "hard",
---     bold = false,
---     italic = {
---       strings = false,
---       comments = false,
---       operators = false,
---       folds = false,
---     },
---   })
---   vim.cmd.colorscheme "gruvbox_material"
--- elseif (theme == "eighties")
--- then
+-- -- main color palette
+-- ---@class EightiesPalette
+--   local Eighties = {}
+--   Eighties.palette = {}
 --   require("eighties").setup({
 --     transparent_mode = true,
 --     bold = false,
@@ -39,11 +29,11 @@ vim.cmd.colorscheme "default"
     style = "night",
     transparent = true,
     on_highlights = function(hl, c)
-      local float_bg = "#1b212c"
-      local prompt_bg = "#212836"
-      hl.Visual = { bg = c.bg_highlight }
-      hl.FloatBorder = { bg = float_bg, fg = c.cyan }
-      hl.NormalFloat = { bg = float_bg }
+      local float_bg             = "#1b212c"
+      local prompt_bg            = "#212836"
+      hl.Visual                  = { bg = c.bg_highlight }
+      hl.FloatBorder             = { bg = float_bg, fg = c.cyan }
+      hl.NormalFloat             = { bg = float_bg }
       -- telescope.nvim
       hl.TelescopeNormal         = { bg = float_bg, fg = c.fg }
       hl.TelescopeBorder         = { bg = float_bg, fg = float_bg }
@@ -54,19 +44,19 @@ vim.cmd.colorscheme "default"
       hl.TelescopePreviewBorder  = { link = "TelescopeBorder" }
       hl.TelescopePreviewLine    = { link = "NormalFloat" }
       hl.TelescopePreviewNormal  = { link = "NormalFloat" }
-      hl.TelescopePreviewTitle   = { bg = c.teal, fg = c.bg_highlight}
+      hl.TelescopePreviewTitle   = { bg = c.teal, fg = c.bg_highlight }
 
       hl.TelescopePrompt         = { link = "TelescopeNormal" }
-      hl.TelescopePromptBorder   = { bg = prompt_bg, fg = prompt_bg}
+      hl.TelescopePromptBorder   = { bg = prompt_bg, fg = prompt_bg }
       hl.TelescopePromptNormal   = { bg = prompt_bg, fg = c.fg_dark }
-      hl.TelescopePromptPrefix   = { bg = prompt_bg, fg = c.cyan}
+      hl.TelescopePromptPrefix   = { bg = prompt_bg, fg = c.cyan }
       hl.TelescopePromptTitle    = { link = "TelescopePreviewTitle" }
 
       hl.TelescopeResultsBorder  = { link = "TelescopeBorder" }
       hl.TelescopeResultsNormal  = { link = "NormalFloat" }
       hl.TelescopeResultsTitle   = { link = "TelescopeBorder" }
 
-      hl.TelescopeSelection      = { bg = c.bg_highlight}
+      hl.TelescopeSelection      = { bg = c.bg_highlight }
       hl.TelescopeSelectionCaret = { fg = c.fg_dark }
     end,
 
