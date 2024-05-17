0c669839c878a535c24e5098f2007f105e0f3286
Mike McBride <mike.mcbride@hey.com>
2024-05-17 14:07:43 -0500

    theme

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ return {
     opts = {
         options = {
             icons_enabled = true,
-            -- theme = "electron_highlighter",
+            theme = "electron_highlighter",
             -- theme = "everforest",
-            theme = "gruvbox_material",
+            -- theme = "gruvbox_material",
             component_separators = '|',
             section_separators = '',
             disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
