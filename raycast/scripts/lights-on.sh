58189b81b2c3a38bd75e5dc84bfc094be3898974
Mike McBride <mike.mcbride@hey.com>
2024-08-21 15:49:36 -0500

    add some raycast scripts, update nvim keymaps

---
+++ raycast/scripts/lights-on.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Lights On
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon ☀️
+# @raycast.packageName Raycast Scripts
+
+litra on -s 2348FE000Y68
+litra on -s 2348FE000YM8
+
