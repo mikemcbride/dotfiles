bbacfb0b053bbbf016e816542b599962ad6a9301
Mike McBride <mike.mcbride@hey.com>
2024-10-31 09:55:06 -0500

    everforest babyyyyyyyyyyyy

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,69 @@
 return {
+    -- {
+    --     'electron-highlighter/nvim',
+    --     name = "electron_highlighter",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {
+    --         transparent = true,
+    --     },
+    --     config = function(_, opts)
+    --         require("electron_highlighter").setup(opts)
+    --         -- vim.cmd.colorscheme "electron_highlighter"
+    --     end
+    -- },
     {
-        'electron-highlighter/nvim',
-        name = "electron_highlighter",
+        "neanias/everforest-nvim",
+        version = false,
         lazy = false,
-        priority = 1000,
+        priority = 1000, -- make sure to load this before all the other start plugins
         opts = {
-            transparent = true,
+            background = "hard",
+            transparent_background_level = 2,
+            colours_override = function(palette)
+                palette.bg_visual = palette.bg3
+                palette.statusline1 = palette.grey1
+                palette.statusline2 = palette.green
+                palette.statusline3 = palette.purple
+            end,
+            on_highlights = function(hl, c)
+                local float_bg = "#2d353b"
+                -- cursors change color based on mode
+                hl.CursorNormal            = { bg = c.fg, fg = c.fg }
+                hl.CursorVisual            = { bg = c.purple, fg = c.purple }
+                hl.CursorInsert            = { bg = c.aqua, fg = c.aqua }
+
+                -- hl.FloatBorder             = { bg = float_bg, fg = c.green }
+
+                -- telescope.nvim
+                hl.TelescopeBorder         = { fg = c.yellow, bg = float_bg }
+                hl.TelescopePreviewBorder  = { link = "TelescopeBorder" }
+                hl.TelescopePreviewTitle   = { link = "TelescopeBorder" }
+                hl.TelescopePromptTitle    = { link = "TelescopeBorder" }
+                hl.TelescopeResultsBorder  = { link = "TelescopeBorder" }
+                hl.TelescopeResultsTitle   = { link = "TelescopeBorder" }
+
+                hl.TelescopeNormal         = { fg = c.fg, bg = float_bg }
+                hl.TelescopePrompt         = { link = "TelescopeNormal" }
+
+                hl.TelescopeMatching       = { fg = c.yellow }
+                hl.TelescopeMultiSelection = { fg = c.fg }
+                hl.TelescopePromptBorder   = { bg = float_bg, fg = c.yellow }
+                hl.TelescopePromptNormal   = { bg = float_bg, fg = c.fg }
+                hl.TelescopePromptPrefix   = { bg = float_bg, fg = c.yellow }
+                hl.TelescopeSelection      = { fg = c.yellow, bg = c.bg_visual }
+                hl.TelescopeSelectionCaret = { fg = c.yellow, bg = c.bg_visual }
+
+                hl.NormalFloat             = { bg = float_bg }
+                hl.TelescopePreview        = { link = "NormalFloat" }
+                hl.TelescopePreviewLine    = { link = "NormalFloat" }
+                hl.TelescopePreviewNormal  = { link = "NormalFloat" }
+                hl.TelescopeResultsNormal  = { link = "NormalFloat" }
+            end
         },
         config = function(_, opts)
-            require("electron_highlighter").setup(opts)
-            vim.cmd.colorscheme "electron_highlighter"
-        end
-    },
+            require("everforest").setup(opts)
+            vim.cmd.colorscheme "everforest"
+        end,
+    }
 }
